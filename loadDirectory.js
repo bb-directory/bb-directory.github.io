eab93d53cc86756823fa71e3782602ef33eeeecb
VictoryTek <136202114+VictoryTek@users.noreply.github.com>
2025-05-08 09:33:42 -0500

    update

--- loadDirectory.js
+++ loadDirectory.js
@@ -21,6 +21,7 @@ document.addEventListener("DOMContentLoaded", function() {
             <td>${project.name}</td>
             <td>${project.creator}</td>
             <td>${project.base}</td>
+            <td>${project.type}</td>
             <td>${project.feedback}</td>
             <td><a href="${project.link}" target="_blank">Visit</a></td>
           `;
